6e773eb01a24e435b954eec8f32bfa83dd08b806
Varshit Tyagi <vksingh1122001@gmail.com>
2025-12-30 14:36:06 +0530

    Restore translation files without secrets

---
+++ backend/controllers/translate.controller.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export const translateMsg = async (req,res) =>{
+    const {message,targetLanguage} = req.body
+    console.log(message);
+    console.log(targetLanguage);
+   
+    const subscriptionKey = process.env.TRANSLATOR_SUBSCRIPTION_KEY;
+    const endpoint = process.env.TRANSLATOR_ENDPOINT;
+    const region = process.env.TRANSLATOR_REGION; 
+
+     try {
+        const response = await axios({
+            method: 'POST',
+            url: `${endpoint}?api-version=3.0&to=${targetLanguage}`,
+            headers: {
+                'Ocp-Apim-Subscription-Key': subscriptionKey,
+                'Ocp-Apim-Subscription-Region': region,
+                'Content-Type': 'application/json'
+            },
+            data: [
+                { Text: message.content }
+            ]
+        });
+
+        // Extract the translated text from the response
+        const translatedText = response.data[0]?.translations[0]?.text;
+        console.log(translatedText);
+
+        if(!translatedText){
+           res.status(400).json({
+            success:false,
+            message:'fail to translate the message'
+           })
+        }
+
+        const translatedMessage = {
+            ...message,
+            content:translatedText
+        }
+
+        // Return translated text
+        res.status(200).json({ 
+            success:true,
+            translatedMessage
+         });
+    } catch (error) {
+        console.error("Translation error:", error);
+        res.status(500).json({ error: "Translation failed" });
+    }
+}
+
+export const getListOfLanguages = async (req,res) =>{
+    
+}
---
+++ frontend/src/Components/Translation.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useDispatch,useSelector } from 'react-redux';
+import { setTargetLanguage } from '../store/lngSlice';
+
+
+
+function Translation() {
+
+    const dispatch = useDispatch();
+
+    const [languages, setLanguages] = useState([])
+    const [lngMenu, setLngMenu] = useState(false);
+ 
+    const targetLanguage = useSelector((state) => state.lng.targetLanguage);
+
+    const setPreferLangauge = (code) =>{
+        dispatch(setTargetLanguage(code));
+        setLngMenu(false);
+    }
+
+    useEffect(() => {
+        const fetchLanguages = async () => {
+            try {
+                const response = await axios.get(import.meta.env.VITE_TRANSLATOR_URL, {
+                    headers: {
+                        'Ocp-Apim-Subscription-Key': import.meta.env.VITE_SUBSCRIPTION_KEY,
+                    },
+                });
+                const langData = response.data.translation;
+                setLanguages(Object.entries(langData));
+                console.log(langData);
+            } catch (error) {
+                console.error('Error fetching languages:', error);
+            }
+        };
+
+        fetchLanguages();
+    }, []);
+
+
+    return (
+
+        <div>
+
+        {    lngMenu     && 
+            <div className='absolute left-[70%] top-[6%] flex flex-col  h-1/4 bg-gray-200 text-sm font-medium rounded-md gap-1 px-1 text-center'>
+                <p className='font-bold text-lg border-b-2 border-black  bg-gray-200'>Select Language</p>
+                <div className=' overflow-y-scroll text-center'>
+                    {
+                        languages.map(([code,language],index) => (
+                            <p key={index} className='hover:bg-gray-600 hover:text-white rounded-lg cursor-pointer' onClick={()=>setPreferLangauge(code)}>{language.name}</p>
+                        ))
+                    }
+                </div>
+            </div>
+}
+
+            <div >
+
+                <FontAwesomeIcon icon={faLanguage} onClick={targetLanguage ? ()=>dispatch(setTargetLanguage(null))  : ()=>setLngMenu(!lngMenu)} className={` ${targetLanguage ? 'text-green-600' : 'text-red-600'} text-2xl `} />
+
+            </div>
+
+        </div>
+
+
+    )
+}
+
+export default Translation
